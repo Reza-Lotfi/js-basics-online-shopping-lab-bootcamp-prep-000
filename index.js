d5c9ff458a0c63bbf2208114ca62d020274b5a6a
Reza Lotfi <rezaiscool7@aol.com>
2018-09-02 01:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var total = total()
-  if (parseInt(cardNumber, 10) === 'NaN'){
+  if (cardNumber >= 0){
     return ("Sorry, we don't have a credit card on file for you.")
     }
   else {
